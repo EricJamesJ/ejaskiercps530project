ed5b60adcbdf93a0690958e160daadb7b7629a2c
ejaskier <ejaskier@scs.ryerson.ca>
2019-11-23 00:08:35 -0500

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { Injectable } from '@angular/core'
-import { Observable } from 'rxjs/Observable'
-import { HttpClient } from '@angular/common/http'
+
 
 @Component({
   selector: 'app-root',
@@ -9,14 +7,8 @@ import { HttpClient } from '@angular/common/http'
   styleUrls: ['./app.component.scss']
 })
 
-export interface Title {
-  name: string
-}
+
 export class AppComponent {
-  constructor(private http: HttpClient) {}
-  getTitle(): Observable<Title> {
-    return this.http.get<Cat[]>('https://ejaskiercps530project.herokuapp.com/api/title')
-  }
+
   title = 'Eric Jaskierski CPS530 Project';
-  title = getTitle().name;
 }
