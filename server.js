e417017e75caf7bb550da58f68dfb4d6b5400dbb
ejaskier <ejaskier@scs.ryerson.ca>
2019-11-23 14:52:36 -0500

    asd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 app.use(express.static(__dirname + '/angular-build'));
-app.get('/', function(req,res){
+app.get('/*', function(req,res){
 res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
 });
 var cat= { name: 'lilly' };
